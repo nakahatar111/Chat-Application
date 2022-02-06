035e17e661236279d7a88ec604fb27bac510e48c
Ryu <49603715+nakahatar111@users.noreply.github.com>
2022-02-06 16:41:54 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Login from './login';
-
+import LoginFn from './login/function_login';
+/*
 ReactDOM.render(
   <React.StrictMode>
     <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+ReactDOM.render(
+  <React.StrictMode>
+    <LoginFn/>
   </React.StrictMode>,
   document.getElementById('root')
 );
